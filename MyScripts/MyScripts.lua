01ca73b6f78f35e770c2b861b602ca5c4c13b4da
Xie Qiu <qiux0518@hotmail.com>
2022-05-08 03:12:44 -0700

    Update MyScripts.lua

--- MyScripts/MyScripts.lua
+++ MyScripts/MyScripts.lua
@@ -1,11 +1,11 @@
 local function MyConfigRaidProfile()
-	--记得把sortgroup设置成始终调用主配置。10人以上的话手动建立一个raid配置。
+    --记得把sortgroup设置成始终调用主配置。10人以上的话手动建立一个raid配置。
 
-	--团队界面配置
+    --团队界面配置
     SetCVar("useCompactPartyFrames",1) --使用团队风格的小队框体界面
     SetCVar("activeCUFProfile","主配置") --主配置
     SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "keepGroupsTogether", false) --保持小队相连
-	SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "useClassColors", true) --显示预计治疗
+    SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "useClassColors", true) --显示预计治疗
     SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "displayHealPrediction", true) --显示预计治疗
     SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "displayPowerBar", true) --显示能量条
     SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "displayAggroHighlight", false) --高亮显示仇恨目标
@@ -32,9 +32,9 @@ local function MyConfigRaidProfile()
     --团队框体位置
     SetRaidProfileSavedPosition(GetActiveRaidProfile(), false, "TOP", 400, "BOTTOM", 50, "LEFT", 850)
 	
-	--团队框体大小
-	SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "frameWidth", 145)
-	SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "frameHeight", 75)
+    --团队框体大小
+    SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "frameWidth", 145)
+    SetRaidProfileOption(CompactUnitFrameProfiles.selectedProfile, "frameHeight", 75)
 
     --刷新设置
     CompactUnitFrameProfiles_SaveChanges(CompactUnitFrameProfiles)
@@ -45,85 +45,88 @@ end
 local f=CreateFrame("frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent",function(self,login,reload)
-	-- 玩家
-	PlayerFrame:ClearAllPoints()
-	PlayerFrame:SetPoint("CENTER", UIParent, "CENTER", -250, -150)
-	PlayerFrame:SetScale(1.0)
-	PlayerFrame:SetUserPlaced(true)
+    -- 玩家
+    PlayerFrame:ClearAllPoints()
+    PlayerFrame:SetPoint("CENTER", UIParent, "CENTER", -250, -150)
+    PlayerFrame:SetScale(1.0)
+    PlayerFrame:SetUserPlaced(true)
 	
-	
-	-- 目标
-	TargetFrame:ClearAllPoints()
-	TargetFrame:SetPoint("CENTER", UIParent, "CENTER", 200, -150)
-	TargetFrame:SetScale(1.0)
-	TargetFrame:SetUserPlaced(true)
+    -- 目标
+    TargetFrame:ClearAllPoints()
+    TargetFrame:SetPoint("CENTER", UIParent, "CENTER", 200, -150)
+    TargetFrame:SetScale(1.0)
+    TargetFrame:SetUserPlaced(true)
 
+    TargetFrameToT:ClearAllPoints()
+    TargetFrameToT:SetScale(1.5)
+    TargetFrameToT:SetPoint("CENTER", UIParent, "CENTER", 230, -95)
+    TargetFrameToT:SetUserPlaced(true)
 
-	TargetFrameToT:SetScale(1.0)
-	TargetFrameToT:SetUserPlaced(false)
+    -- 焦点
+    FocusFrame:ClearAllPoints()
+    FocusFrame:SetPoint("CENTER", UIParent, "CENTER", -425, 375)
+    FocusFrame:SetScale(1.0)
+    FocusFrame:SetUserPlaced(true)
+    FocusFrameSpellBar:SetScale(1.6)
+	
+    -- Buff
+    BuffFrame:ClearAllPoints()
+    BuffFrame:SetPoint("CENTER", UIParent, "CENTER", 550, 550)
+	
+    --隐藏头像受到伤害和治疗数字
+    local p=PlayerHitIndicator; p.Show=p.Hide; p:Hide()
+    local p=PetHitIndicator; p.Show=p.Hide; p:Hide()
+		
+    -- 自己施法条
+    CastingBarFrame:ClearAllPoints()
+    CastingBarFrame:SetPoint("CENTER", WorldFrame, "CENTER", -25, -250)
+    CastingBarFrame.Icon:Show()				
+    CastingBarFrame.Icon:SetSize(26,26)		
+    --CastingBarFrame:SetUserPlaced(true)
+		
+    LossOfControlFrame:SetScale(1.3)
+    LossOfControlFrame.RedLineTop:SetAlpha(0)
+    LossOfControlFrame.RedLineBottom:SetAlpha(0)
+    LossOfControlFrame.blackBg:SetAlpha(0)
 
-
-	-- 焦点
-	FocusFrame:ClearAllPoints()
-	FocusFrame:SetPoint("CENTER", UIParent, "CENTER", -425, 375)
-	FocusFrame:SetScale(1.0)
-	FocusFrame:SetUserPlaced(true)
-	FocusFrameSpellBar:SetScale(1.6)
+    MainMenuBarArtFrame.LeftEndCap:Hide()
+    MainMenuBarArtFrame.RightEndCap:Hide()
 	
+    -- 姓名版相关
+    SetCVar("nameplateGlobalScale", 1.1)		-- 全局
+    SetCVar("nameplateSelectedScale", 1.2)		-- 选中缩放，默认1.2
+    SetCVar("NamePlateHorizontalScale", 1.4)		-- 大姓名版
+    SetCVar("NamePlateVerticalScale", 2.7)		-- 大姓名版
+    SetCVar("UnitNameEnemyPlayerName", 1)		-- 显示敌人姓名，配合后面的姓名版显示arena1，2，3
 	
-	-- Buff
-	BuffFrame:ClearAllPoints()
-	BuffFrame:SetPoint("CENTER", UIParent, "CENTER", 550, 550)
+    -- PVP姓名版
+    -- 0  overlapping 	/堆叠 	/允许覆盖
+    -- 1  stacking    	/折叠 	/不允许覆盖
+    SetCVar("nameplateMotion", 0)			-- 允许重叠
+    SetCVar("nameplateOverlapV",  1.1)			-- 越小，姓名版允许离的越近
+    SetCVar("nameplateOverlapH",  0.8)
 	
+    SetCVar("nameplateShowEnemyGuardians", 0) 		-- 守護者
+    SetCVar("nameplateShowEnemyMinions", 1)  		-- 僕從
+    SetCVar("nameplateShowEnemyPets", 1)  		-- 寵物
+    SetCVar("nameplateShowEnemyTotems", 1) 		-- 圖騰
+    SetCVar("nameplateShowEnemyMinus", 1) 		-- 次要
 	
-	--隐藏头像受到伤害和治疗数字
-	local p=PlayerHitIndicator; p.Show=p.Hide; p:Hide()
-	local p=PetHitIndicator; p.Show=p.Hide; p:Hide()
+    SetCVar("threatShowNumeric", 0) 			-- 目标姓名版仇恨数字
 	
+    SetCVar("WorldTextScale", 1.5) 			-- 战斗字体
+    SetCVar("profanityFilter",0) 			-- 语言过滤器
+    SetCVar("secureAbilityToggle", 1) 			-- 关闭自动取消冰箱，潜行，etc
+    SetCVar("nameplateShowSelf",0) 			-- 显示个人资源
+    SetCVar("nameplateShowAll",1) 			-- 显示所有姓名板
+    SetCVar("nameplateShowSelf",0) 			-- 不显示个人资源
+    SetCVar("alwaysCompareItems", 0) 			-- 比较装备
+    SetCVar("ShowClassColorInNameplate", 1) 		-- 姓名版职业颜色
+    SetCVar('ShowClassColorInFriendlyNameplate', 1) 	-- 友方姓名版颜色
 	
-	-- 自己施法条
-	CastingBarFrame:ClearAllPoints()
-	CastingBarFrame:SetPoint("CENTER", WorldFrame, "CENTER", -25, -250)
-	--CastingBarFrame:SetUserPlaced(true)
-	
-	
-	LossOfControlFrame:SetScale(1.3)
-	LossOfControlFrame.RedLineTop:SetAlpha(0)
-	LossOfControlFrame.RedLineBottom:SetAlpha(0)
-	LossOfControlFrame.blackBg:SetAlpha(0)
-
-	MainMenuBarArtFrame.LeftEndCap:Hide()
-	MainMenuBarArtFrame.RightEndCap:Hide()
-	
-	
-	-- PVP姓名版
-	-- 0  overlapping /堆叠 	/允许覆盖
-	-- 1  stacking    /折叠 /不允许覆盖
-	SetCVar("nameplateMotion", 0)				-- 允许重叠
-	SetCVar("nameplateOverlapV",  1.1)			-- 越小，姓名版允许离的越近
-	SetCVar("nameplateOverlapH",  0.8)
-	
-	SetCVar("WorldTextScale", 1.5) -- 战斗字体
-	SetCVar("profanityFilter",0) --语言过滤器
-	SetCVar("secureAbilityToggle", 1) -- 关闭自动取消冰箱，潜行，etc
-	SetCVar("nameplateShowSelf",0) --显示个人资源
-	SetCVar("nameplateShowAll",1) --显示所有姓名板
-	SetCVar("nameplateShowSelf",0) --不显示个人资源
-	SetCVar("alwaysCompareItems", 0) -- 比较装备
-	SetCVar("ShowClassColorInNameplate", 1) -- 姓名版职业颜色
-	SetCVar('ShowClassColorInFriendlyNameplate', 1) -- 友方姓名版颜色
-	
-	SetCVar("nameplateShowEnemyGuardians", 0) --守護者
-	SetCVar("nameplateShowEnemyMinions", 1)  --僕從
-	SetCVar("nameplateShowEnemyPets", 1)  --寵物
-	SetCVar("nameplateShowEnemyTotems", 1) --圖騰
-	SetCVar("nameplateShowEnemyMinus", 1) --次要
-	
-	SetCVar("threatShowNumeric", 0) --目标姓名版仇恨数字
-	
-	if ( GetNumGroupMembers() < 5) then 
-		MyConfigRaidProfile()
-	end
+    if ( GetNumGroupMembers() < 5) then 
+	    MyConfigRaidProfile()
+    end
 end)
 
 -- 竞技场敌对框体123
@@ -141,13 +144,94 @@ end)
 
 -- 竞技场隐藏队友名字
 hooksecurefunc("CompactUnitFrame_UpdateName",function()
-	local name
-	for i = 1,3 do
-		name = _G["CompactRaidFrame"..i.."Name"]
-		-- print(name)
+    local name
+    for i = 1,3 do
+	name = _G["CompactRaidFrame"..i.."Name"]
+	-- print(name)
 
-		if name and IsActiveBattlefieldArena() then
-			name:Hide()
-		end
+	if name and IsActiveBattlefieldArena() then
+	    name:Hide()
 	end
+    end
 end)
+
+--血条右侧显示箭头
+hooksecurefunc("CompactUnitFrame_UpdateSelectionHighlight", function(frame)
+if frame:IsForbidden() then return end
+if frame.unit:lower():match("nameplate") then
+    if not frame.arrow then
+	frame.arrow = frame.healthBar:CreateTexture(nil, "PARENT")
+	frame.arrow:SetTexture("Interface\\Addons\\MyScripts\\feather.tga")		-- 图标路径
+	frame.arrow:SetPoint("left", frame.healthBar, "left", -40, 0)			-- 右方箭头位置
+	frame.arrow:SetSize(40, 20)							-- 箭头大小
+	frame.arrow:Hide()
+    end
+    if UnitIsUnit(frame.displayedUnit, "target") then
+        frame.arrow:Show()
+    else
+	frame.arrow:Hide()
+    end
+    if (UnitIsUnit("player", frame.unit) or UnitIsFriend("player", "target")) and frame.arrow then
+	frame.arrow:Hide()
+    end
+end
+end)
+
+--血条变色
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+frame:RegisterEvent("PLAYER_TARGET_CHANGED")
+frame:RegisterEvent("UNIT_THREAT_LIST_UPDATE")
+frame:RegisterEvent("UNIT_HEALTH")
+frame:SetScript("OnEvent", function(self, event, frame)
+for i, namePlate in ipairs(C_NamePlate.GetNamePlates()) do
+
+    local unitFrame = namePlate.UnitFrame
+    if unitFrame:IsForbidden() then return end
+
+    if not UnitIsPlayer(unitFrame.unit) and not UnitIsTapDenied(unitFrame.unit) and C_NamePlate.GetNamePlateForUnit(unitFrame.unit) ~= C_NamePlate.GetNamePlateForUnit("player") then
+	local reaction = UnitReaction(unitFrame.unit, "player")
+	local guid = UnitGUID(unitFrame.unit)
+	local _, _, _, _, _, id = strsplit("-", guid or "")
+	local pt = UnitIsUnit(unitFrame.unit.."target", UnitName("player"))
+
+	if green[tonumber(id)] then
+	    unitFrame.healthBar:SetStatusBarColor(0, 1, 0, 1)	-- 特殊血条 绿色
+	    unitFrame.healthBar:SetHeight(30)
+	elseif hide[tonumber(id)] then
+	    unitFrame.healthBar:Hide()
+        elseif ((id == "134389") or (id == "134390") or (id == "174773")or (id == "170483")) and pt == true then
+	    unitFrame.healthBar:SetStatusBarColor(0.3, 0, 0.6, 1) 	-- 怨毒
+	elseif reaction == 4 then
+	    unitFrame.healthBar:SetStatusBarColor(1, 1, 0, 1) 	-- 中立怪 黄色
+	elseif reaction >= 5 then
+	    unitFrame.healthBar:SetStatusBarColor(0, 1, 0, 1)	-- 友方npc 绿色
+	else
+	    unitFrame.healthBar:SetStatusBarColor(1, 0, 0, 1)	-- 其他 红色	
+	end
+    end
+end
+end)
+
+green = {
+    [120651] = true,		-- 易爆球
+    [119052] = true, 		-- 战旗
+    [5925] = true, 		-- 根基
+    [5913] = true,		-- 战栗
+    [53006] = true, 		-- 灵魂链接
+    [59764] = true, 		-- 治疗之潮
+    [105427] = true,		-- 天怒
+    [61245] = true,		-- 电能
+    [105451] = true,		-- 反击
+    [166523] = true,		-- 墓钟
+    [179867] = true,		-- 静电立场
+    [101398] = true,		-- 灵能魔
+    -- [180113] = true, 	-- 喋喋不休的法师
+    [179193] = true, 		-- 邪能方尖碑
+    [179733] = true, 		-- 鱼串
+}
+
+hide = {
+    [95072] = true,		-- 巨型土元素
+    [5394] = true, 		-- 治疗之泉 测试
+}
